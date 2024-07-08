c3380a91ebb4048180f2193ccbcabcd01920bed9
sumaiyashifa <134805287+sumaiyashifa@users.noreply.github.com>
2024-07-08 17:51:48 +0600

    studentmanagement final push

---
+++ src/main/java/com/mycompany/studentmanagement/Studentmanagement.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studentmanagement;
+
+/**
+ *
+ * @author Asus
+ */
+public class Studentmanagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
